08fbf841ebe39489c428948133fb898177921aa2
kbsjsn <shin.kirk@gmail.com>
2019-06-17 12:21:08 -0700

    Implements getNextUniqueId

--- datastore/counter.js
+++ datastore/counter.js
@@ -38,11 +38,14 @@ const writeCounter = (count, callback) => {
 
 // Public API - Fix this function //////////////////////////////////////////////
 
-exports.getNextUniqueId = () => {
-  counter = counter + 1;
-  return zeroPaddedNumber(counter);
+exports.getNextUniqueId = (callback) => {
+  readCounter((err, id) => {
+    id++;
+    writeCounter(id, (err, counterString) => {
+      callback(null, counterString);
+    })
+  });
 };
-
 
 
 // Configuration -- DO NOT MODIFY //////////////////////////////////////////////
